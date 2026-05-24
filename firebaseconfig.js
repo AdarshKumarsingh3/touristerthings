225a23893c6df7a0bf9c1b1827729aaf3feec47d
AdarshKumarsingh3 <adarshkr.singh10569@gmail.com>
2026-05-24 14:20:31 +0530

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyDrN-VTUtZGRi-MImjxQ8Ukl1pF3ZZ8qfs",
+  authDomain: "touristerthings.firebaseapp.com",
+  projectId: "touristerthings",
+  storageBucket: "touristerthings.firebasestorage.app",
+  messagingSenderId: "886077092436",
+  appId: "1:886077092436:web:eece2ecc4e207c40c933c1"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
